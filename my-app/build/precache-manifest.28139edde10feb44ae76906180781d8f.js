76158563764f704212020097b077d8ee5e0144ff
rauldimon <56930720+rauldimon@users.noreply.github.com>
2020-02-11 00:34:32 +0200

    444

---
+++ my-app/build/precache-manifest.28139edde10feb44ae76906180781d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d3a3d5412afa51ba0602b55c7c431b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4fbb524cd72ae0230f",
+    "url": "/static/css/main.c5c94866.chunk.css"
+  },
+  {
+    "revision": "66237dee91e4b5d0625d",
+    "url": "/static/js/2.12033191.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.12033191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4fbb524cd72ae0230f",
+    "url": "/static/js/main.ace12faf.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/static/media/252.e45d9d1f.png"
+  }
+]);
